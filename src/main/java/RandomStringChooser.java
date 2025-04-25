07ffddc97309b0735e28358fd07291186c6373a0
clovisZ <165089300+clovisZ@users.noreply.github.com>
2025-04-24 22:53:48 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class RandomStringChooser
 {
   /* to be implemented in part (a) */
-  Arraylist<String> words = mew Arraylist<>();
+  Arraylist<String> words = new Arraylist<>();
   
   public RandomStringChooser(String[] arrayWords) {
     for (int i = 0; i < arrayWords.length; i++) 
